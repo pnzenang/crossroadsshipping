4b5cd014eeed01c8b142ef341c18586075706a77
pnzenang <pnzenang@gmail.com>
2026-01-17 16:44:48 -0500

    again

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -14,13 +14,12 @@ function Sidebar() {
         const variant = isActivePage ? 'default' : 'ghost';
         return (
           <Button
+            key={link.href}
             asChild
             className='w-full mb-2 capitalize font-normal justify-start'
             variant={variant}
           >
-            <Link key={link.href} href={link.href}>
-              {link.label}
-            </Link>
+            <Link href={link.href}>{link.label}</Link>
           </Button>
         );
       })}
