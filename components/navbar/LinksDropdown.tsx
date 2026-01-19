8809af7a97a5aa2a58f47d1f5d63f28148e3e420
pnzenang <pnzenang@gmail.com>
2026-01-19 07:23:12 -0500

    1.19.commit

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -4,19 +4,19 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
   DropdownMenuSeparator,
-} from '@/components/ui/dropdown-menu'
-import { LuAlignLeft } from 'react-icons/lu'
-import Link from 'next/link'
-import { Button } from '../ui/button'
-import { links } from '@/utils/links'
-import UserIcon from './UserIcon'
-import { SignInButton, SignedIn, SignedOut, SignUpButton } from '@clerk/nextjs'
-import SignOutLink from './SignOutLink'
-import { auth } from '@clerk/nextjs/server'
-import { FaAlignLeft } from 'react-icons/fa'
+} from '@/components/ui/dropdown-menu';
+import { LuAlignLeft } from 'react-icons/lu';
+import Link from 'next/link';
+import { Button } from '../ui/button';
+import { links } from '@/utils/links';
+import UserIcon from './UserIcon';
+import { SignInButton, SignedIn, SignedOut, SignUpButton } from '@clerk/nextjs';
+import SignOutLink from './SignOutLink';
+import { auth } from '@clerk/nextjs/server';
+import { FaAlignLeft } from 'react-icons/fa';
 function LinksDropdown() {
-  const { userId } = auth()
-  const isAdmin = userId === process.env.ADMIN_USER_ID
+  const { userId } = auth();
+  const isAdmin = userId === process.env.ADMIN_USER_ID;
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -33,27 +33,27 @@ function LinksDropdown() {
       <DropdownMenuContent className='w-40' align='start' sideOffset={10}>
         <SignedOut>
           <DropdownMenuItem>
-            <SignInButton mode='modal'>
+            <SignInButton mode='redirect'>
               <button className='w-full text-left'>Login</button>
             </SignInButton>
           </DropdownMenuItem>
           <DropdownMenuSeparator />
           <DropdownMenuItem>
-            <SignUpButton mode='modal'>
+            <SignUpButton mode='redirect'>
               <button className='w-full text-left'>Register</button>
             </SignUpButton>
           </DropdownMenuItem>
         </SignedOut>
         <SignedIn>
           {links.map((link) => {
-            if (link.label === 'dashboard' && !isAdmin) return null
+            if (link.label === 'dashboard' && !isAdmin) return null;
             return (
               <DropdownMenuItem key={link.href}>
                 <Link href={link.href} className='capitalize w-full'>
                   {link.label}
                 </Link>
               </DropdownMenuItem>
-            )
+            );
           })}
           <DropdownMenuSeparator />
           <DropdownMenuItem>
@@ -62,6 +62,6 @@ function LinksDropdown() {
         </SignedIn>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
-export default LinksDropdown
+export default LinksDropdown;
