c367f1bda0de689705cc90c65ed562b09b930ce7
pnzenang <pnzenang@gmail.com>
2026-02-08 16:00:45 -0500

    2.8.commit.2

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,13 @@ import LoadingContainer from '@/components/global/LoadingContainer';
 import FeaturedProducts from '@/components/home/FeaturedProducts';
 import Hero from '@/components/home/Hero';
 import Hero2 from '@/components/home/Hero2';
+import { Metadata } from 'next';
 import { Suspense } from 'react';
+export const metadata: Metadata = {
+  title: 'crossroads shipping',
+  description: 'A nifty way to ship your items to Africa',
+};
+
 function HomePage() {
   return (
     <section className='pt-32'>
